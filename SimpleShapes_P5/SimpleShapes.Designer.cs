6f6cd4ead52053ec24b49b0400c0803d9c3bac25
derrk <derrik.pollock@gmail.com>
2022-10-16 14:23:39 -0500

    added textbox for points (x,y)

--- SimpleShapes_P5/SimpleShapes.Designer.cs
+++ SimpleShapes_P5/SimpleShapes.Designer.cs
@@ -35,6 +35,19 @@
             this.P2box = new System.Windows.Forms.GroupBox();
             this.P3box = new System.Windows.Forms.GroupBox();
             this.P4box = new System.Windows.Forms.GroupBox();
+            this.DrawButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.P1box.SuspendLayout();
+            this.P2box.SuspendLayout();
+            this.P3box.SuspendLayout();
+            this.P4box.SuspendLayout();
             this.SuspendLayout();
             // 
             // ShapeComboBox
@@ -58,39 +71,112 @@
             // 
             // P1box
             // 
+            this.P1box.Controls.Add(this.textBox2);
+            this.P1box.Controls.Add(this.textBox1);
             this.P1box.Location = new System.Drawing.Point(30, 83);
             this.P1box.Name = "P1box";
-            this.P1box.Size = new System.Drawing.Size(144, 46);
+            this.P1box.Size = new System.Drawing.Size(144, 62);
             this.P1box.TabIndex = 1;
             this.P1box.TabStop = false;
             this.P1box.Text = "Point 1";
             // 
             // P2box
             // 
-            this.P2box.Location = new System.Drawing.Point(30, 146);
+            this.P2box.Controls.Add(this.textBox4);
+            this.P2box.Controls.Add(this.textBox3);
+            this.P2box.Location = new System.Drawing.Point(30, 159);
             this.P2box.Name = "P2box";
-            this.P2box.Size = new System.Drawing.Size(144, 46);
+            this.P2box.Size = new System.Drawing.Size(144, 62);
             this.P2box.TabIndex = 2;
             this.P2box.TabStop = false;
             this.P2box.Text = "Point 2";
             // 
             // P3box
             // 
-            this.P3box.Location = new System.Drawing.Point(30, 198);
+            this.P3box.Controls.Add(this.textBox6);
+            this.P3box.Controls.Add(this.textBox5);
+            this.P3box.Location = new System.Drawing.Point(30, 236);
             this.P3box.Name = "P3box";
-            this.P3box.Size = new System.Drawing.Size(144, 46);
+            this.P3box.Size = new System.Drawing.Size(144, 70);
             this.P3box.TabIndex = 2;
             this.P3box.TabStop = false;
             this.P3box.Text = "Point 3";
             // 
             // P4box
             // 
-            this.P4box.Location = new System.Drawing.Point(30, 250);
+            this.P4box.Controls.Add(this.textBox8);
+            this.P4box.Controls.Add(this.textBox7);
+            this.P4box.Location = new System.Drawing.Point(30, 321);
             this.P4box.Name = "P4box";
-            this.P4box.Size = new System.Drawing.Size(144, 46);
+            this.P4box.Size = new System.Drawing.Size(144, 59);
             this.P4box.TabIndex = 2;
             this.P4box.TabStop = false;
             this.P4box.Text = "Point 4";
+            // 
+            // DrawButton
+            // 
+            this.DrawButton.Location = new System.Drawing.Point(30, 401);
+            this.DrawButton.Name = "DrawButton";
+            this.DrawButton.Size = new System.Drawing.Size(144, 39);
+            this.DrawButton.TabIndex = 3;
+            this.DrawButton.Text = "Draw";
+            this.DrawButton.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(43, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(75, 36);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(43, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(6, 36);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(43, 20);
+            this.textBox3.TabIndex = 1;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(75, 36);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(43, 20);
+            this.textBox4.TabIndex = 2;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(75, 44);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(43, 20);
+            this.textBox5.TabIndex = 1;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(6, 44);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(43, 20);
+            this.textBox6.TabIndex = 2;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(75, 33);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(43, 20);
+            this.textBox7.TabIndex = 1;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(6, 33);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(43, 20);
+            this.textBox8.TabIndex = 2;
             // 
             // SimpleShapes
             // 
@@ -98,6 +184,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonShadow;
             this.ClientSize = new System.Drawing.Size(840, 479);
+            this.Controls.Add(this.DrawButton);
             this.Controls.Add(this.P4box);
             this.Controls.Add(this.P3box);
             this.Controls.Add(this.P2box);
@@ -105,6 +192,14 @@
             this.Controls.Add(this.ShapeComboBox);
             this.Name = "SimpleShapes";
             this.Text = "Form1";
+            this.P1box.ResumeLayout(false);
+            this.P1box.PerformLayout();
+            this.P2box.ResumeLayout(false);
+            this.P2box.PerformLayout();
+            this.P3box.ResumeLayout(false);
+            this.P3box.PerformLayout();
+            this.P4box.ResumeLayout(false);
+            this.P4box.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -116,6 +211,15 @@
         private System.Windows.Forms.GroupBox P2box;
         private System.Windows.Forms.GroupBox P3box;
         private System.Windows.Forms.GroupBox P4box;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button DrawButton;
     }
 }
 
